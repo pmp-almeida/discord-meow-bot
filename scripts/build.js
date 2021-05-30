49e33798d2b1e2c9c8caa6b347c13fd41f15eae2
Pedro Almeida <pmp.almeida@campus.fct.unl.pt>
2021-05-30 21:23:41 +0100

    Build scripts Linux + Windows redone

--- scripts/build.js
+++ scripts/build.js
@@ -1,71 +1,61 @@
 const exec = require('child_process').exec;
 const os = require('os');
 
-function puts(error, stdout, stderr) {
-	if(error) {
-		if(error.code == 127 && error.cmd == 'mongo -version');
-		throw 'mongoNotFound';
-	} else if(stdout) {
-		if(!stdout.startsWith('MongoDB shell version v')) {
-			console.log(stdout);
-		}
-	}
-}
+function puts(error, stdout) { console.log(stdout); }
 
 // MONGODB SETUP
-function mongodb(op_sys) {
-	console.log('--------------------------------------------');
-	try {
-		exec('mongo -version', puts);
-		console.log('MongoDB already installed. Skipping.');
-	} catch(e) {
+// windows
+function mongodb_win_handler(error) {
+	if(error && error.code == 127 && error.cmd == 'mongo -version') {
 		console.log('Downloading and installing MongoDB. This may take a while...');
-		// Windows
-		if (op_sys == 0) {
-			exec('powershell.exe "curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6-signed.msi -outfile mongodb.msi"', puts);
-			exec('msiexec.exe /l*v mdbinstall.log  /qb /i mongodb-windows-x86_64-4.4-signed.msi SHOULD_INSTALL_COMPASS="0"', puts);
-			// Mac
-		} else if (op_sys == 1) {
-			// TODO
-			// Linux
-		} else if (op_sys == 2) {
-			const linux_version = os.version().split('~')[1].substring(0, 5);
-			exec('wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -', puts);
-			if (linux_version === '20.04') {
-				exec('echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list', puts);
-			} else if (linux_version === '18.04') {
-				exec('echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list', puts);
-			} else if (linux_version === '16.04') {
-				exec('echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list', puts);
-			}
-		}
-		exec('sudo apt-get update', puts);
-		exec('sudo apt-get install -y mongodb-org', puts);
-		exec('echo "mongodb-org hold" | sudo dpkg --set-selections', puts);
-		exec('echo "mongodb-org-server hold" | sudo dpkg --set-selections', puts);
-		exec('echo "mongodb-org-shell hold" | sudo dpkg --set-selections', puts);
-		exec('echo "mongodb-org-mongos hold" | sudo dpkg --set-selections', puts);
-		exec('echo "mongodb-org-tools hold" | sudo dpkg --set-selections', puts);
-		console.log('MongoDB setup done.');
+		exec('powershell.exe "curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6-signed.msi -outfile mongodb.msi" &&\
+msiexec.exe /l*v mdbinstall.log  /qb /i mongodb-windows-x86_64-4.4-signed.msi SHOULD_INSTALL_COMPASS="0" &&\
+\nMongoDB setup complete.', puts);
+	} else {
+		console.log('MongoDB already installed. Skipping.');
 	}
 }
 
+// linux
+function mongodb_lin_handler(error) {
+	if(error && error.code == 127 && error.cmd == 'mongo -version') {
+		console.log('Downloading and installing MongoDB. This may take a while...');
+		const linux_version = os.version().split('~')[1].substring(0, 5);
+		let url_string;
+		if (linux_version === '20.04') {
+			url_string = 'focal';
+		} else if (linux_version === '18.04') {
+			url_string = 'bionic';
+		} else if (linux_version === '16.04') {
+			url_string = 'xenial';
+		}
+		exec(`wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add - &&\
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${url_string}/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list &&\
+sudo apt-get update &&\
+sudo apt-get install -y mongodb-org &&\
+echo "mongodb-org hold" | sudo dpkg --set-selections &&\
+echo "mongodb-org-server hold" | sudo dpkg --set-selections &&\
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections &&\
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections &&\
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections &&\
+echo "\nMongoDB setup complete."`, puts);
+	} else {
+		console.log('MongoDB already installed. Skipping.');
+	}
+}
 
 console.log('Executing first time setup...');
 // Run command depending on the OS
 if (os.type() === 'Windows_NT') {
 	console.log('Running on a Windows based OS');
-	mongodb(0);
+	exec('mongo -version', mongodb_win_handler);
 } else if (os.type() === 'Darwin') {
 	console.log('Running on a macOS based OS');
-	mongodb(1);
+	console.log('Automatic setup not supported. Please check README.md');
+	// exec('mongo -version', mongodb_handler);
 } else if (os.type() === 'Linux') {
 	console.log('Running on Linux based OS');
-	mongodb(2);
+	exec('mongo -version', mongodb_lin_handler);
 } else {
 	throw new Error('Unsupported OS found: ' + os.type());
 }
-
-console.log('--------------------------------------------');
-console.log('Installing third-part dependencies...');
-exec('npm install', puts);
