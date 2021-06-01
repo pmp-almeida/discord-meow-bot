8196dabc39eba16a84db5695de2801b63ae22e2c
As400 <p.miguel96@hotmail.com>
2021-06-01 16:37:18 +0100

    Windows build script typo fixed

--- scripts/build.js
+++ scripts/build.js
@@ -6,11 +6,13 @@ function puts(error, stdout) { console.log(stdout); }
 // MONGODB SETUP
 // windows
 function mongodb_win_handler(error) {
-	if(error && error.code == 127 && error.cmd == 'mongo -version') {
+	if(error && error.code == 1 && error.cmd == 'mongo -version') {
 		console.log('Downloading and installing MongoDB. This may take a while...');
-		exec('powershell.exe "curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6-signed.msi -outfile mongodb.msi" &&\
-msiexec.exe /l*v mdbinstall.log  /qb /i mongodb-windows-x86_64-4.4-signed.msi SHOULD_INSTALL_COMPASS="0" &&\
-\nMongoDB setup complete.', puts);
+	exec('powershell "curl https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-4.4.6-signed.msi -outfile mongodb.msi" &&\
+msiexec.exe /l*v mdbinstall.log  /qb /i mongodb.msi SHOULD_INSTALL_COMPASS="0" &&\
+powershell -Command "Start-Process %cd%/set_env_variables.bat -Verb RunAs" &&\
+set PATH="%PATH%;C:\Program Files\MongoDB\Server\4.4\bin" &&\
+echo MongoDB setup complete.', puts);
 	} else {
 		console.log('MongoDB already installed. Skipping.');
 	}
